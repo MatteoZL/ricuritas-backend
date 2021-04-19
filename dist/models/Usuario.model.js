9500b02b3a5d6ba5d9a92f7e4859971d7eb05a4a
Matt <tutorialesmatteo@gmail.com>
2021-04-19 11:52:11 -0500

    Version 1.1

--- dist/models/Usuario.model.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-//# sourceMappingURL=Usuario.model.js.map
--- dist/routes/auth.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const auth_ctrler_1 = require("../controllers/auth.ctrler");
-const router = express_1.Router();
-router.post("/signup", auth_ctrler_1.signup);
-router.post("/signin", auth_ctrler_1.signin);
-exports.default = router;
-//# sourceMappingURL=auth.routes.js.map
