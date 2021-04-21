c27bd160aa7137db7610e1385b528e9296a52d06
MatteoZL <matteo.zuluaga@correounivalle.edu.co>
2021-04-21 15:36:19 -0500

    Heroku database

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,13 +1,17 @@
 import { Sequelize } from "sequelize-typescript";
 
-export const db = new Sequelize("ricuritas", "postgres", "password", {
-  host: "localhost",
-  dialect: "postgres",
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000,
+export const db = new Sequelize(
+  "d4bjg5tms14g14",//"ricuritas", 
+  "tyqljyhijyqfyi", //"postgres", 
+  "fa9fd456d22298918137a83316ddadb3fa40f4e41637adf04633e71e339036b7", //"password", 
+  {
+    host: "ec2-18-215-111-67.compute-1.amazonaws.com",//"localhost",
+    dialect: "postgres",
+    pool: {
+     max: 5,
+     min: 0,
+     acquire: 30000,
+     idle: 10000,
   },
   //logging: false,
 });
